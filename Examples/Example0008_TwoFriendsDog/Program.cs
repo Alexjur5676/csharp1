0f51f0c954e8c6fe4782a5f4922a21c918aff6d0
Alexjur5676 <alex9992142@mail.ru>
2022-08-20 20:10:30 +0700

    Add Examples

---
+++ Examples/Example0008_TwoFriendsDog/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("firstFriendSpeed: ");
+double firstFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("secondFriendSpeed: ");
+double secondFriendSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("dogSpeed: ");
+double dogSpeed = Convert.ToDouble(Console.ReadLine());
+Console.Write("distance: ");
+double distance = Convert.ToDouble(Console.ReadLine());
+int count = 0;
+int friend = 2;
+double time = 0;
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+    Console.Write("distance = ");
+    Console.WriteLine(distance);
+    Console.Write("time = ");
+    Console.WriteLine(time);
+}
+Console.Write("count = ");
+Console.WriteLine(count);
---
+++ Examples/Methods/Program.cs
@@ -0,0 +1,79 @@
+﻿// Вид
+void Method1()
+{
+    Console.WriteLine("Автор Журавлев А.В.");
+}
+Method1();
+
+// // Вид 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// // Method2(msg:"Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// // Method21("Текст", 4);
+// // или
+// // Method21(msg: "Текст", count: 4);
+// // или
+// // Method21( count: 4, msg: "новый текст");
+
+
+// // Вид 3
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method3();
+// // Console.WriteLine(year);
+
+// // Вид 4
+// // string Method4(int count, string text)
+// // {
+// //     int i = 0;
+// //     string result = String.Empty;
+// //     while (i < count)
+// //     {
+// //         result = result + text;
+// //         i++;
+// //     }
+// //     return result;
+// // }
+// // string result = Method4(10, "abcd");
+// // Console.WriteLine(result);
+
+// // или
+
+// // string Method4(int count, string text)
+// // {
+// //     string result = String.Empty;
+// //     for (int i = 0; i < count; i++)
+// //     {
+// //         result = result + text;
+
+// //     }
+// //     return result;
+// // }
+// // string result = Method4(10, "abcd");
+// // Console.WriteLine(result);
+
+// // Цикл в цикле(таблица умножения) 
+// for (int i =2;i <=10; i++)
+// {
+//     for(int j =2;j <=10; j++)
+//     {
+//         Console.WriteLine($"{i}*{j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
