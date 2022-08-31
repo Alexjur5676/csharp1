6cab39fae10d63d4974bc4988394fd4efb2839dd
Alexjur5676 <alex9992142@mail.ru>
2022-08-31 14:13:21 +0700

    Add ExampleRec_Prod_1

--- Examples/ExampleCode_Prav_003/Program.cs
+++ Examples/ExampleCode_Prav_003/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿С помощью этих методов можно просматривать файлы на устройствах.
+// void CatalogInfo(string path,string indent = "")
+// {
+//     DirectoryInfo catalog = new DirectoryInfo(path);
+//     DirectoryInfo[] catalogs = catalog.GetDirectories();
+//     for (int i = 0; i < catalogs.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{catalogs[i].Name}");
+//         CatalogInfo(catalogs[i].FullName, indent + " ");
+//     }
+//     FileInfo[] files = catalog. GetFiles();
+//     for (int i = 0; i < files.Length; i++)
+//     {
+//         Console.WriteLine($"{indent}{files[i].Name}");
+//     }
+// }
+// string path = @"C:\Users\Марина\Desktop\csharp1\Examples\Example001_NewConsole"; 
+// CatalogInfo(path);
+
+string path = "/Users/Марина/Desktop/csharp1/Examples/Example001_NewConsole";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for(int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
